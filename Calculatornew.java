eef089b03c13e85c38392f330c15baa3649b3940
sundarik0010 <99658769+sundarik0010@users.noreply.github.com>
2022-02-21 22:02:30 +0530

    Add files via upload

---
+++ Calculatornew.java
@@ -0,0 +1,20 @@
+package com.tute.classes;
+
+public class Calculatornew {
+	public static void main(String[] args) {
+        
+       double a=12;
+		double b = 2;
+		double c = a*b;
+		System.out.println("c= "+c);
+		
+		float e=10.5f;  
+        float f=5f;  
+        float g=e/f;
+        System.out.println("g="+g);  
+        	
+        
+        
+            }
+
+}
---
+++ characterOccurance.java
@@ -0,0 +1,22 @@
+package com.tute.classes;
+
+public class characterOccurance {
+	public static void main(String[] args) {
+
+		String str="welcome to Chennai";
+
+		int count=0;
+		
+
+		for (int i=0; i< str.length(); i++) {
+
+		if(str.charAt(i)=='e') {
+		count++;
+		}
+		}
+		System.out.println('e'+" appears "+count+" times in the given statement");
+		}
+		}
+
+
+
